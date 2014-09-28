20060661dd92c9efc3958b4825f0ddf4f2248e5e
ivacbka <pol-ii@ya.ru>
2014-09-29 01:51:56 +0400

    updated code formatting

--- LibraryTest.cs
+++ LibraryTest.cs
@@ -20,7 +20,8 @@ namespace LibraryTest
 				if(!IsTestPassed(_sets[i]))
 				{
 					passed = false;
-					Console.WriteLine("Test failed on dataset[" + i + "]:" + " side A:" + _sets[i].A + " side B:" + _sets[i].B + " side C:" + _sets[i].C);
+					Console.WriteLine("Test failed on dataset[" + i + "]:" + " side A:" 
+						+ _sets[i].A + " side B:" + _sets[i].B + " side C:" + _sets[i].C);
 				}
 			}
 			
@@ -46,7 +47,8 @@ namespace LibraryTest
 		{
 			try
 			{
-				bool resultIsRight = MathUtils.AreAlmostEqual(AreaUtils.RightTriangleArea( dataSet.A, dataSet.B, dataSet.C), dataSet.Area, _relError, _absError);
+				bool resultIsRight = MathUtils.AreAlmostEqual(
+					AreaUtils.RightTriangleArea( dataSet.A, dataSet.B, dataSet.C), dataSet.Area, _relError, _absError);
 				return (resultIsRight && dataSet.Result == TestResult.Succeded);
 			}
 			catch(AreaUtils.InvalidInputException)
@@ -82,16 +84,16 @@ namespace LibraryTest
 
 		private static List<TestDataSet> _sets = new List<TestDataSet>()
 		{
-			new TestDataSet{ A = 0, 								B=3, C=4, Result=TestResult.Failed, 	Area=0},
-			new TestDataSet{ A = -3, 								B=4, C=5, Result=TestResult.Failed, 	Area=0},
-			new TestDataSet{ A = 2, 								B=4, C=5, Result=TestResult.Failed, 	Area=0},
-			new TestDataSet{ A = 3 + _absError - Single.Epsilon, 	B=4, C=5, Result=TestResult.Succeded, 	Area=6.0f},
-			new TestDataSet{ A = 3, 								B=4, C=5, Result=TestResult.Succeded, 	Area=6.0f},
-			new TestDataSet{ A = Single.NaN, 						B=4, C=5, Result=TestResult.Failed, 	Area=0},
-			new TestDataSet{ A = Single.NegativeInfinity, 			B=4, C=5, Result=TestResult.Failed, 	Area=0},
-			new TestDataSet{ A = Single.PositiveInfinity, 			B=4, C=5, Result=TestResult.Failed, 	Area=0},
-			new TestDataSet{ A = Single.MaxValue, 					B=4, C=5, Result=TestResult.Failed, 	Area=0},
-			new TestDataSet{ A = 30000 + 30000 * _relError * 0.99f, B=40000, C=50000, Result=TestResult.Succeded, 	Area=600000000.0f},
+			new TestDataSet{ A = 0, 								B = 3, 		C = 4, 		Result = TestResult.Failed, 	Area = 0},
+			new TestDataSet{ A = -3, 								B = 4, 		C = 5, 		Result = TestResult.Failed, 	Area = 0},
+			new TestDataSet{ A = 2, 								B = 4, 		C = 5, 		Result = TestResult.Failed, 	Area = 0},
+			new TestDataSet{ A = 3 + _absError - Single.Epsilon,	B = 4,		C = 5, 		Result = TestResult.Succeded, 	Area = 6.0f},
+			new TestDataSet{ A = 3, 								B = 4, 		C = 5, 		Result = TestResult.Succeded, 	Area = 6.0f},
+			new TestDataSet{ A = Single.NaN, 						B = 4, 		C = 5,		Result = TestResult.Failed, 	Area = 0},
+			new TestDataSet{ A = Single.NegativeInfinity, 			B = 4, 		C = 5, 		Result = TestResult.Failed, 	Area = 0},
+			new TestDataSet{ A = Single.PositiveInfinity, 			B = 4, 		C = 5, 		Result = TestResult.Failed, 	Area = 0},
+			new TestDataSet{ A = Single.MaxValue, 					B = 4, 		C = 5, 		Result = TestResult.Failed, 	Area = 0},
+			new TestDataSet{ A = 30000 + 30000 * _relError * 0.99f, B = 40000, 	C = 50000, 	Result = TestResult.Succeded, 	Area = 600000000.0f},
 		};
 
 		#endregion
